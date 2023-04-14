388c538af34086e470a5836d8d34d721e59e29c3
Ghazi <ghazialhouwari@gmail.com>
2023-04-15 01:18:01 +0200

    feat(GrapesJS): add GrapesJS with basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<template>
-  <v-app>
-    <v-main>
-      
-    </v-main>
-  </v-app>
-</template>
-
 <script setup lang="ts">
-  
+	import GrapesJSEditor from './components/GrapesJSEditor.vue';
 </script>
+
+<template>
+	<v-app>
+		<v-main>
+			<v-container>
+				<GrapesJSEditor />
+			</v-container>
+		</v-main>
+	</v-app>
+</template>
